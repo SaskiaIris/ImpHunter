e22209646d660fd1c3c0aa2fa5cf8d943548d792
IrisSaskia <irisoostra@upcmail.nl>
2019-05-11 14:23:27 +0200

    Muis volgen
    Het kanon volgt nu de muis (alle trial & errors nog in comments).

--- ImpHunter/GameStates/PlayingState.cs
+++ ImpHunter/GameStates/PlayingState.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using System;
+using System.Diagnostics;
+using Microsoft.Xna.Framework.Input;
 
 namespace ImpHunter {
     class PlayingState : GameObjectList{
@@ -45,7 +47,7 @@ namespace ImpHunter {
 			//cannon.Barrel.Angle = (float)Math.Atan2(cannon.Barrel.Position.Y - crosshair.Position.Y, -1 * (cannon.Barrel.Position.X - crosshair.Position.X));
 			//cannon.Barrel.Angle = (float)Math.Atan2(crosshair.Position.Y - cannon.Barrel.Origin.Y, crosshair.Position.X - cannon.Barrel.Origin.Y);
 
-			if(crosshair.Position.X > cannon.Barrel.Origin.X)
+			/*if(crosshair.Position.X > cannon.Barrel.Origin.X)
 			{
 				rotationVectorBarrel.X = crosshair.Position.X - cannon.Barrel.Origin.X;
 			} else
@@ -61,23 +63,72 @@ namespace ImpHunter {
 			} else
 			{
 				rotationVectorBarrel.Y = cannon.Barrel.Origin.Y - crosshair.Position.Y;
+			}*/
+
+			if (crosshair.Position.X >= cannon.Position.X && crosshair.Position.Y >= cannon.Position.Y)
+			{
+				//TWEEDE KWADRANT
+				rotationVectorBarrel.X = crosshair.Position.X - cannon.Position.X;
+				rotationVectorBarrel.Y = crosshair.Position.Y - cannon.Position.Y;
+				//rotationVectorBarrel.Y *= -1;
+				//cannon.Barrel.Angle = (float)(Math.Atan2(rotationVectorBarrel.Y, rotationVectorBarrel.X) + (180 * 180 / 3.14));
+				cannon.Barrel.Angle = (float)(Math.Atan2(rotationVectorBarrel.Y, rotationVectorBarrel.X) + (3.14/2));
+			}
+			else if (crosshair.Position.X >= cannon.Position.X && crosshair.Position.Y < cannon.Position.Y)
+			{
+				//EERSTE KWADRANT
+				rotationVectorBarrel.X = crosshair.Position.X - cannon.Position.X;
+				rotationVectorBarrel.Y = cannon.Position.Y - crosshair.Position.Y;
+				//rotationVectorBarrel.X *= -1;
+				//rotationVectorBarrel.Y *= -1;
+				cannon.Barrel.Angle = (float)(Math.Atan2(-1 * rotationVectorBarrel.Y, rotationVectorBarrel.X) + (3.14/2));
+			}
+			else if (crosshair.Position.X < cannon.Position.X && crosshair.Position.Y >= cannon.Position.Y)
+			{
+				//DERDE KWADRANT
+				rotationVectorBarrel.X = cannon.Position.X - crosshair.Position.X;
+				rotationVectorBarrel.Y = crosshair.Position.Y - cannon.Position.Y;
+				//rotationVectorBarrel.Y = cannon.Position.Y - crosshair.Position.Y;
+				cannon.Barrel.Angle = (float)(Math.Atan2(rotationVectorBarrel.Y, -1 * rotationVectorBarrel.X) + (3.14 / 2));
+			}
+			else if (crosshair.Position.X < cannon.Position.X && crosshair.Position.Y < cannon.Position.Y)
+			{
+				//VIERDE KWADRANT
+				rotationVectorBarrel.X = cannon.Position.X - crosshair.Position.X;
+				rotationVectorBarrel.Y = cannon.Position.Y - crosshair.Position.Y;
+				//rotationVectorBarrel.X *= -1;
+				//rotationVectorBarrel.Y *= -1;
+				//cannon.Barrel.Angle = (float)(Math.Atan2(rotationVectorBarrel.Y, rotationVectorBarrel.X) * -1);
+				cannon.Barrel.Angle = (float)(Math.Atan2(-1 * rotationVectorBarrel.Y, -1 * rotationVectorBarrel.X) + (3.14 / 2));
+			}
+			else
+			{
+				//rotationVectorBarrel = new Vector2(1, 1);
 			}
 
-			if(crosshair.Position.X < cannon.Position.X)
+			/*Debug.WriteLine("cannon.Barrel.Origin.X = ", cannon.Barrel.Origin.X.ToString());
+			Debug.WriteLine("cannon.Barrel.Origin.Y = ", cannon.Barrel.Origin.Y.ToString());
+			Debug.WriteLine("crosshair.Position.X = ", crosshair.Position.X.ToString());
+			Debug.WriteLine("crosshair.Position.Y = ", crosshair.Position.Y.ToString());
+			Debug.WriteLine("rotationVectorBarrel.X = ", rotationVectorBarrel.X.ToString());
+			Debug.WriteLine("rotationVectorBarrel.Y = ", rotationVectorBarrel.Y.ToString());
+			Debug.WriteLine("cannonBarrelAngle = ", cannon.Barrel.Angle.ToString());*/
+
+			/*if(crosshair.Position.X < cannon.Position.X)
 			{
 				cannon.Barrel.Angle = (float)Math.Atan2(-1 * rotationVectorBarrel.Y, rotationVectorBarrel.X);
 			} else
-			{
-				cannon.Barrel.Angle = (float)Math.Atan2(rotationVectorBarrel.Y, rotationVectorBarrel.X);
-			}
+			{*/
 
-			if(crosshair.Position.X == cannon.Position.X)
+			//}
+
+			/*if(crosshair.Position.X == cannon.Position.X)
 			{
 				cannon.Barrel.Angle = 0f;
-			}
+			}*/
 
-			
-        }
+
+		}
 
         /// <summary>
         /// Allows the player to shoot after a cooldown.
@@ -92,6 +143,30 @@ namespace ImpHunter {
                 crosshair.Expand(SHOOT_COOLDOWN);
                 shootTimer = 0;
             }
+
+			if(inputHelper.KeyPressed(Keys.Space))
+			{
+				/*Debug.WriteLine(" = cannon.Barrel.Origin.X", cannon.Barrel.Origin.X.ToString());
+				Debug.WriteLine(" = cannon.Barrel.Origin.Y", cannon.Barrel.Origin.Y.ToString());
+				Debug.WriteLine(" ");
+				Debug.WriteLine(" = cannon.Barrel.Position.X", cannon.Barrel.Position.X.ToString());
+				Debug.WriteLine(" = cannon.Barrel.Position.Y", cannon.Barrel.Position.Y.ToString());
+				Debug.WriteLine(" ");*/
+				Debug.WriteLine(" = cannon.Position.X", cannon.Position.X.ToString());
+				Debug.WriteLine(" = cannon.Position.Y", cannon.Position.Y.ToString());
+				Debug.WriteLine(" ");
+				Debug.WriteLine(" = inputHelper.MousePosition.X", inputHelper.MousePosition.X.ToString());
+				Debug.WriteLine(" = inputHelper.MousePosition.Y", inputHelper.MousePosition.Y.ToString());
+				Debug.WriteLine(" ");
+				Debug.WriteLine(" = crosshair.Position.X", crosshair.Position.X.ToString());
+				Debug.WriteLine(" = crosshair.Position.Y", crosshair.Position.Y.ToString());
+				Debug.WriteLine(" ");
+				Debug.WriteLine(" = rotationVectorBarrel.X", rotationVectorBarrel.X.ToString());
+				Debug.WriteLine(" = rotationVectorBarrel.Y", rotationVectorBarrel.Y.ToString());
+				Debug.WriteLine(" ");
+				Debug.WriteLine(" = cannon.Barrel.Angle", cannon.Barrel.Angle.ToString());
+				Debug.WriteLine(" ");
+			}
 		}
     }
 }
